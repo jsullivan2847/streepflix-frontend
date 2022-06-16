00cff0e95a69ed112bebbc8c80868a1fd891a661
James Sullivan <jsullivan2847@gmail.com>
2022-06-16 12:01:33 -0400

    started working on API data flow

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,37 @@
 import Movies from "../components/Movies";
+import { useState, useEffect } from "react";
 
 const Dashboard = () => {
-    return(
-        <div className="dashboard-container">
-            <Movies/>
-        </div>
-    )
-}
+  const [movieState, setMovieState] = useState(null);
+
+  const API_KEY = "98e3fb1f";
+
+  const getMovie = async (searchTerm) => {
+    //send request to API
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${API_KEY}&t=mamma-mia`
+    );
+    //stores response from API in data variable
+    const data = await response.json();
+
+    //sets movie state with the data retrieved
+    setMovieState(data);
+  };
+
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+
+
+  console.log(movieState);
+
+  return (
+    <div className="dashboard-container">
+      <Movies />
+    </div>
+  );
+};
 
 export default Dashboard;
