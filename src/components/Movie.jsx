291042e9a5143f0f40a3c9b2333b639c03acd888
James Sullivan <jsullivan2847@gmail.com>
2022-06-16 14:50:57 -0400

    movie components display the poster and title

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,7 +1,10 @@
 const Movie = ({movie}) => {
+    const posterURL = 'http://image.tmdb.org/t/p/w185'
+    const {title, poster_path, backdrop_path, release_date} = movie;
     console.log(movie);
     return(
         <div className="movie-container">
+            <img src={`${posterURL}${poster_path}`} alt={`${title} poster`} />
             <h5>{movie.title}</h5>
         </div>
     )
