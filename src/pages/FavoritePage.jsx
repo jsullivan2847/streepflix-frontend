b48272fca01c7e97a949e31973103db0364d6c7e
kemd <kemd91@gmail.com>
2022-06-22 12:11:42 -0400

    adds favorite and favorite local storage

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import Nav from '../components/Nav'
+import Movie from '../components/Movie'
 
-const FavoritePage = () => {
-  return (
-    <div>
-        <Nav />
-        <h1>my Favorites</h1>
-    </div>
+const FavoritePage = ({favorites, movie, getTrailer, displayTrailer, updateFavorites}) => {
+
+  const DisplayFavorites = () => {
+    return favorites.map((movie, index) => {
+      return  <Movie getTrailer={getTrailer}displayTrailer={displayTrailer} movie={movie} key={index} updateFavorites={updateFavorites}/>
+  } 
   )
+}
+
+  return DisplayFavorites()
 }
 
 export default FavoritePage
