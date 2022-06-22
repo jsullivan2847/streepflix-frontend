af0c0e5475d59a67306e89c6ddbd09d6b8cac7c0
Me <104526114+bmather02@users.noreply.github.com>
2022-06-22 12:58:55 -0400

    adds loading spinner to userselect page and adds styling to add to favorites button

--- src/pages/UserSelect.jsx
+++ src/pages/UserSelect.jsx
@@ -4,24 +4,31 @@ const UserSelect = (props) => {
         props.selectUser(e.target.attributes.id.nodeValue)
         props.history.push("/")
     }
-
-    return(
-        <div className="user-select-container">
-            <h1>Select A User</h1>
-            {props.profiles.map(profile => (
-                <div className="user" key = {profile._id}>
-                    <img className="posters-container"src={profile.image} onClick={handleClick} id={profile._id}></img>
-                    {/* <button onClick={handleClick} id={profile._id}>Use This Account</button> */}
-                    <div className="user-controls">
-                        <h3>{profile.name}</h3>
-                    <Link to={`${profile._id}/edit`}>
-                        <h3 className="posters-container mirror"> &#9998;</h3>
-                    </Link>
-                    </div>
-                    </div>
-            ))}
-            <Link to="/new"><h1><span className="add-user-link">+ Add User</span></h1></Link>
+    const loaded = () => {
+        return <div className="user-select-container">
+        <h1>Select A User</h1>
+        {props.profiles.map(profile => (
+            <div className="user" key = {profile._id}>
+                <img className="posters-container"src={profile.image} onClick={handleClick} id={profile._id}></img>
+                {/* <button onClick={handleClick} id={profile._id}>Use This Account</button> */}
+                <div className="user-controls">
+                    <h3>{profile.name}</h3>
+                <Link to={`${profile._id}/edit`}>
+                    <h3 className="posters-container mirror"> &#9998;</h3>
+                </Link>
+                </div>
+                </div>
+        ))}
+        <Link to="/new"><h1><span className="add-user-link">+ Add User</span></h1></Link>
+    </div>
+    }
+    const loading = () => {
+        return <div className="spinner">
         </div>
+    }
+    return(
+        props.profiles ? loaded(): loading()
+       
     )
 }
 
